a4e58949b590301373368713b8b0bd91a320190f
joseph <j.hernandez.92189@gmail.com>
2022-08-22 16:26:55 -0700

    refactored code

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,99 +2,109 @@ var apiKey = "758d6634aa9f49adb412c47ed584b5ac";
 var currentCityContainer = document.querySelector("#current-city")
 var form = document.querySelector("#form-control");
 var searchCity = document.querySelector("#search-city");
+var globalLat = ""
+var globalLon = ""
+var apiUrl = "";
+var uvUrl = "";
 
-var formSubmit = function (event) {
-    event.preventDefault();
+var getCord = function (searchParam) {
+    // utilizing this function to use the eventual "searchInput" to get coordinates based off of search
+    var geocoding = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchParam + "&limit=5&appid=" + apiKey;
 
-    searchInput = searchCity.value.trim();
-    console.log(searchInput);
-
-    var geocoding = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchInput + "&limit=5&appid=" + apiKey;
-
-    fetch(geocoding).then(function (response) {
+    var coordinates = fetch(geocoding).then(function (response) {
         if (response.ok) {
-            response.json().then(function (data) {
+            var coordinates = response.json().then(function (data) {
                 var lat = data[0].lat;
                 var lon = data[0].lon;
 
-                var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
-                
+                var cordArr = {
+                    lattitude: lat,
+                    longitude: lon
+                }
+                return (cordArr);
+            })
+        }
+        return (coordinates);
+    });
+    return (coordinates);
+};
+// var coordinates = getCord("vancouver").then(function(data){
+//     return(data);
+// })
 
-                var getUrlData = function () {
+var formSubmit = function (event) {
+    event.preventDefault();
+    searchInput = searchCity.value.trim();
+    getCord(searchInput).then(function (data) {
+        console.log(data.lattitude);
+        globalLat = data.lattitude;
+        globalLon = data.longitude;
+        apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + globalLat + "&lon=" + globalLon + "&appid=" + apiKey + "&units=imperial";
+        displayCurrentWeather();
+        uvUrl = "http://api.openweathermap.org/data/2.5/air_pollution?lat=" + globalLat + "&lon=" + globalLon + "&appid=" + apiKey + "&units=imperial";
+    });
 
-                    fetch(apiUrl).then(function (response) {
+}
 
-                        if (response.ok) {
-                            response.json().then(function (data) {
-                                console.log(data);
+var displayCurrentWeather = function () {
 
-                                var currentCity = document.createElement("div")
-                                currentCity.setAttribute("id", "current-city-selector")
-                                currentCityContainer.appendChild(currentCity);
-                        
-                                // date conversion source https://www.coderrocketfuel.com/article/convert-a-unix-timestamp-to-a-date-in-vanilla-javascript
-                                var unix = data.dt * 1000;
-                                var currentDate = new Date(unix);
-                                var humanDate = currentDate.toLocaleDateString()
+    fetch(apiUrl).then(function (response) {
 
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
 
-                                var cityName = document.createElement("h3");
-                                cityName.classList = "fw-bolder"
-                                cityName.textContent = data.name + " " + humanDate;
-                                currentCity.appendChild(cityName);
+                var currentCity = document.createElement("div")
+                currentCity.setAttribute("id", "current-city-selector")
+                currentCityContainer.appendChild(currentCity);
 
-                                var iconCode = data.weather[0].icon;
-                                var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
-                                var displayIcon = document.createElement("img");
-                                displayIcon.setAttribute("src", iconUrl);
-                                currentCity.appendChild(displayIcon);
+                // date conversion source https://www.coderrocketfuel.com/article/convert-a-unix-timestamp-to-a-date-in-vanilla-javascript
+                var unix = data.dt * 1000;
+                var currentDate = new Date(unix);
+                var humanDate = currentDate.toLocaleDateString()
 
 
+                var cityName = document.createElement("h3");
+                cityName.classList = "fw-bolder"
+                cityName.textContent = data.name + " " + humanDate;
+                currentCity.appendChild(cityName);
 
-                                var currentTemp = document.createElement("p");
-                                currentTemp.textContent = "Current Temp: " + data.main.temp;
-                                currentCity.appendChild(currentTemp);
+                var iconCode = data.weather[0].icon;
+                var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+                var displayIcon = document.createElement("img");
+                displayIcon.setAttribute("src", iconUrl);
+                currentCity.appendChild(displayIcon);
 
-                                var currentHumid = document.createElement("p");
-                                currentHumid.textContent = "Current Humidity: " + data.main.humidity;
-                                currentCity.appendChild(currentHumid);
 
-                                var currentWS = document.createElement("p");
-                                currentWS.textContent = "Current Wind Speed: " + data.wind.speed + " MPH";
-                                currentCity.appendChild(currentWS);
 
-                                var uvUrl = "http://api.openweathermap.org/data/2.5/air_pollution?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
+                var currentTemp = document.createElement("p");
+                currentTemp.textContent = "Current Temp: " + data.main.temp;
+                currentCity.appendChild(currentTemp);
 
-                                var getUVData = function () {
-                                    fetch(uvUrl).then(function (response) {
-                                        if (response.ok) {
-                                            response.json().then(function (data) {
-                                                console.log(data.list[0].main.aqi);
-                
-                                                var currentUV = document.createElement("p");
-                                                currentUV.textContent = "Current UV: " + data.list[0].main.aqi;
-                                                currentCity.appendChild(currentUV);
-                                            })
-                                        }
-                                    })
-                                };
-                                getUVData();
-                            })
-                        };
-                    });
-                };
+                var currentHumid = document.createElement("p");
+                currentHumid.textContent = "Current Humidity: " + data.main.humidity;
+                currentCity.appendChild(currentHumid);
 
-                // UV data
+                var currentWS = document.createElement("p");
+                currentWS.textContent = "Current Wind Speed: " + data.wind.speed + " MPH";
+                currentCity.appendChild(currentWS);
 
-                getUrlData();
+                fetch(uvUrl).then(function (response) {
+                    if (response.ok) {
+                        response.json().then(function (data) {
+                            console.log(data.list[0].main.aqi);
 
-                currentCityContainer.remove(currentCity);
+                            var currentUV = document.createElement("p");
+                            currentUV.textContent = "Current UV: " + data.list[0].main.aqi;
+                            currentCity.appendChild(currentUV);
+                        })
+                    }
+                })
+
             })
         }
     })
 }
-
-
 
 form.addEventListener('submit', formSubmit);
 
